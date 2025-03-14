9cc14bc66ff59fc639b482532c4c7ede0951c7b7
yobaNGE <recum12233@mail.ru>
2025-03-14 13:58:33 +0300

    meh

--- discovery-service/src/main/java/org/chiches/config/EurekaServerSecurityConfig.java
+++ discovery-service/src/main/java/org/chiches/config/EurekaServerSecurityConfig.java
@@ -12,15 +12,10 @@ public class EurekaServerSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Disable CSRF for simplicity in dev
                 .csrf(csrf -> csrf.disable())
-                // Configure request authorization
                 .authorizeHttpRequests(authorize -> authorize
-                        // Allow access to Eureka endpoints and actuator
                         .requestMatchers("/eureka/**", "/actuator/**").permitAll()
-                        // Any other requests need to be authenticated
                         .anyRequest().authenticated())
-                // Optionally enable HTTP Basic authentication if needed
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
